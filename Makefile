ea25cf8e5db9e3888dd5e1615aa8b682d46149e2
Frank Hunleth <fhunleth@troodon-software.com>
2013-07-23 10:28:23 -0400

    Basic code builds and runs

--- Makefile
+++ Makefile
@@ -1,36 +1,29 @@
-CROSS_COMPILE?=../buildroot/output/host/usr/bin/arm-buildroot-linux-uclibcgnueabi-
+CC=../buildroot/output/host/usr/bin/arm-buildroot-linux-uclibcgnueabi-gcc
 PASM?=../buildroot/output/host/usr/bin/pasm
 
 LIBDIR_APP_LOADER?=../buildroot/staging/usr/lib
 INCDIR_APP_LOADER?=../buildroot/staging/usr/include
-BINDIR?=.
 
 CFLAGS+= -Wall -I$(INCDIR_APP_LOADER) -D__DEBUG -O2 -mtune=cortex-a8 -march=armv7-a
 LDFLAGS+=-L$(LIBDIR_APP_LOADER) -lprussdrv -lpthread
 OBJDIR=obj
-TARGET=$(BINDIR)/PRU_memAccess_DDR_PRUsharedRAM
+TARGET=PRU_memAccess_DDR_PRUsharedRAM
 
-_DEPS = 
-DEPS = $(patsubst %,$(INCDIR_APP_LOADER)/%,$(_DEPS))
+DEPS = PRU_memAccess_DDR_PRUsharedRAM_bin.h
+OBJ = PRU_memAccess_DDR_PRUsharedRAM.o
 
-_OBJ = PRU_memAccess_DDR_PRUsharedRAM.o
-OBJ = $(patsubst %,$(OBJDIR)/%,$(_OBJ))
+all: $(TARGET)
+$(OBJ): $(DEPS)
 
-all: $(TARGET) PRU_memAccess_DDR_PRUsharedRAM.bin
-
-%.bin: %.p
-	$(PASM) -b $<
-
-$(OBJDIR)/%.o: %.c $(DEPS)
-	@mkdir -p obj
-	$(CROSS_COMPILE)gcc $(CFLAGS) -c -o $@ $< 
+%_bin.h: %.p
+	$(PASM) -c $<
 
 $(TARGET): $(OBJ)
-	$(CROSS_COMPILE)gcc $(CFLAGS) -o $@ $^ $(LDFLAGS)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 
 .PHONY: clean
 
 clean:
-	rm -rf $(OBJDIR)/ *~  $(TARGET) *.bin
+	rm -rf *.o *~  $(TARGET) *_bin.h
 
--- PRU_memAccess_DDR_PRUsharedRAM.c
+++ PRU_memAccess_DDR_PRUsharedRAM.c
@@ -1,47 +1,3 @@
-/*
- * PRU_memAccess_DDR_PRUsharedRAM.c
- *
- * Copyright (C) 2012 Texas Instruments Incorporated - http://www.ti.com/ 
- * 
- * 
- *  Redistribution and use in source and binary forms, with or without 
- *  modification, are permitted provided that the following conditions 
- *  are met:
- *
- *    Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer.
- *
- *    Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the 
- *    documentation and/or other materials provided with the   
- *    distribution.
- *
- *    Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
- *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
-*/
-
-/*
- * ============================================================================
- * Copyright (c) Texas Instruments Inc 2010-12
- *
- * Use of this software is controlled by the terms and conditions found in the
- * license agreement under which this software has been supplied or provided.
- * ============================================================================
- */
 
 /******************************************************************************
 * PRU_memAccess_DDR_PRUsharedRAM.c
@@ -72,6 +28,8 @@
 // Driver header file
 #include "prussdrv.h"
 #include <pruss_intc_mapping.h>	 
+
+#include "PRU_memAccess_DDR_PRUsharedRAM_bin.h"
 
 /******************************************************************************
 * Explicit External Declarations                                              *
@@ -149,11 +107,13 @@ int main (void)
 
     /* Initialize example */
     printf("\tINFO: Initializing example.\r\n");
-    LOCAL_exampleInit(PRU_NUM);
+    LOCAL_exampleInit();
     
     /* Execute example on PRU */
     printf("\tINFO: Executing example.\r\n");
-    prussdrv_exec_program (PRU_NUM, "./PRU_memAccess_DDR_PRUsharedRAM.bin");
+    prussdrv_pru_disable(PRU_NUM);
+    prussdrv_pru_write_memory(PRUSS0_PRU0_IRAM, 0, PRUcode, sizeof(PRUcode));
+    prussdrv_pru_enable(PRU_NUM);
 
     /* Wait until PRU0 has finished execution */
     printf("\tINFO: Waiting for HALT command.\r\n");
